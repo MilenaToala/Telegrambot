ff074b3e1483a1a17c7e1f958dc4b47ba8e5f8eb
MilenaToala <77599325+MilenaToala@users.noreply.github.com>
2021-03-16 20:02:31 -0500

    Update Registros.py

--- Registros.py
+++ Registros.py
@@ -3,14 +3,14 @@ import random as rd
 
 class Recorrido:
     def registrar(self, destino, hora, fecha, origen):
-        self.aero1 = origen
-        self.aero2 = destino
+        self.aero_1 = origen
+        self.aero_2 = destino
         self.hora = hora
         self.fecha = fecha
-        self.asientos = 100
+        self.asientos = 180
 
     def mostrar(self):
-        return "Lugar de LLegada: " + self.aero2.mostrar() + ", fecha de llegada: " + self.fecha.isoformat() +", hora de llegada: " + self.hora.isoformat()
+        return "Lugar de LLegada: " + self.aero_2.mostrar() + ", fecha de llegada: " + self.fecha.isoformat() +", hora de llegada: " + self.hora.isoformat()
 
 
 class Aeropuerto:
@@ -24,19 +24,19 @@ class Aeropuerto:
 
     def nuevos_vuelos(self, aeroptos, min_valor = 4, max_valor = 8):
         for i in range( 0, rd.randrange(min_valor, max_valor)):
-            aeropuerto12 = self
+            aeropuerto_12 = self
 
-            while aeropuerto12 == self:
-                aeropuerto12 = aeroptos[rd.randrange(0, len(aeroptos))]
+            while aeropuerto_12 == self:
+                aeropuerto_12 = aeroptos[rd.randrange(0, len(aeroptos))]
 
             hora = datetime.time(rd.randrange(0, 24), rd.randrange(0, 60))
             fecha = datetime.date.today() + datetime.timedelta(days= rd.randrange(1, 16))
-            avion = Recorrido(aeropuerto12, hora, fecha, self)
+            avion = Recorrido(aeropuerto_12, hora, fecha, self)
             self.fly.append(avion)
 
     def borrar(self):
         for avion in self.fly:
-            print("Aeropuerto: " + avion.aero2)
+            print("Aeropuerto: " + avion.aero_2)
 
     def mostrar(self):
         return "Aeropuerto: " + self.nombre + ", país: " + self.pais
